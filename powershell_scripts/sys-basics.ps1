70458672b6af44d8955eafee7169ad2314a965b6
Carson Scohera <155030761+cscohera@users.noreply.github.com>
2025-09-22 10:44:45 -0400

    Fixing script
    Scripts had incorrect way to get winget

--- powershell_scripts/sys-basics.ps1
+++ powershell_scripts/sys-basics.ps1
@@ -6,15 +6,13 @@ if (Get-Command winget -ErrorAction SilentlyContinue) {
 } else {
     Write-Host "winget is not installed"
     $progressPreference = 'silentlyContinue'
-    $latestWingetMsixBundleUri = $(Invoke-RestMethod https://api.github.com/repos/microsoft/winget-cli/releases/latest).assets.browser_download_url | Where-Object {$_.EndsWith(".msixbundle")}
+    $latestWingetMsixBundleUri = $( Invoke-RestMethod https://api.github.com/repos/microsoft/winget-cli/releases/latest ).assets.browser_download_url | Where-Object { $_.EndsWith( ".msixbundle" ) }
     $latestWingetMsixBundle = $latestWingetMsixBundleUri.Split("/")[-1]
     Write-Information "Downloading winget to artifacts directory..."
-    Invoke-WebRequest -Uri $latestWingetMsixBundleUri -OutFile "./$latestWingetMsixBundle"
-    Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx
-    Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx
+    Invoke-WebRequest -Uri $latestWingetMsixBundleUri -OutFile "./$latestWingetMsixBundle" 
     Add-AppxPackage $latestWingetMsixBundle
 }
 
-winget install "Sysinternals Suite" --accept-package-agreements
+winget install "Sysinternals Suite" --accept-source-agreements --accept-package-agreements
 
 winget list "Sysinternals Suite"
