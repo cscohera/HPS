1f62531b3a5625157a8f00a3936905d8a3db84b3
Carson Scohera <155030761+cscohera@users.noreply.github.com>
2025-09-24 17:20:01 -0400

    updates to sys-basics script

--- powershell_scripts/sys-basics.ps1
+++ powershell_scripts/sys-basics.ps1
@@ -8,6 +8,8 @@ if (Get-Command choco -ErrorAction SilentlyContinue) {
     [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 }
 
-choco install sysinternals -y
+# NEED TO INSTALL GIT choco install git.install -y
+choco install sysinternals -y --params "/InstallDir:C:\"
 choco install python3 -y
-
+choco install everything -y --params "/start-menu-shortcuts /run-on-system-startup" 
+pip install watchdog
