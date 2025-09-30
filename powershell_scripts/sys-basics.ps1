bc70963dc4e520eeb55fd548dad37559820231d0
Carson Scohera <155030761+cscohera@users.noreply.github.com>
2025-09-29 22:57:00 -0400

    update sys-basics

--- powershell_scripts/sys-basics.ps1
+++ powershell_scripts/sys-basics.ps1
@@ -1,4 +1,4 @@
-#check to see if microsoft fixed hash mismatch issue
+
 Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force
 
 if (Get-Command choco -ErrorAction SilentlyContinue) {
@@ -8,10 +8,12 @@ if (Get-Command choco -ErrorAction SilentlyContinue) {
     [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 }
 
+#maybe bluespawn or commodo kill switch
 Set-SmbServerConfiguration -EnableSMB1Protocol -Confirm:$false
 
+Invoke-WebRequest -Uri https://github.com/ION28/BLUESPAWN/releases/download/v0.5.1-alpha/BLUESPAWN-client-x64.exe -OutFile "C:\Blue\BLUESPAWN-client-x64.exe"
 # NEED TO INSTALL GIT choco install git.install -y
-choco install sysinternals -y --params "/InstallDir:C:\"
+choco install sysinternals -y --params "/InstallDir:C:\SSD\"
 choco install python3 -y
 choco install everything -y --params "/start-menu-shortcuts /run-on-system-startup" 
-pip install watchdog
+#install watchdog
