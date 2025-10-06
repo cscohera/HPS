0fbd5651ffdb93d5a43c6433388355c38c32e44d
Carson Scohera <155030761+cscohera@users.noreply.github.com>
2025-10-06 17:22:16 -0400

    adding more asr rules

--- powershell_scripts/gpo2022.ps1
+++ powershell_scripts/gpo2022.ps1
@@ -53,10 +53,21 @@ Write-Host "Enabled key audit policies"
 # =============== CONFIGURE DEFENDER ASR RULES ===============
 # Ensure Defender is installed and running
 $asrRules = @(
-    "56a863a9-875e-4185-98a7-b882c64b5ce5",  # Block Office apps from creating child processes
-    "3b576869-a4ec-4529-8536-b80a7769e899",  # Block credential stealing from LSASS
-    "D4F940AB-401B-4EFC-AADC-AD5F3C50688A",  # Block executable content from email/webmail
-    "26190899-1602-49e8-8b27-eb1d0a1ce869"   # Block process creation from PSExec and WMI
+
+    "56A863A9-875E-4185-98A7-B882C64B5CE5" = "Block abuse of exploited vulnerable signed drivers";
+    "7674BA52-37EB-4A4F-A9A1-F0F9A1619A2C" = "Block Adobe Reader from creating child processes";
+    "D4F940AB-401B-4EFC-AADC-AD5F3C50688A" = "Block all Office applications from creating child processes";
+    "9E6C4E1F-7D60-472F-BA1A-A39EF669E4B2" = "Block credential stealing from the Windows local security authority subsystem (lsass.exe)";
+    "BE9BA2D9-53EA-4CDC-84E5-9B1EEEE46550" = "Block executable content from email client and webmail";
+    "D3E037E1-3EB8-44C8-A917-57927947596D" = "Block JavaScript or VBScript from launching downloaded executable content";
+    "3B576869-A4EC-4529-8536-B80A7769E899" = "Block Office applications from creating executable content";
+    "75668C1F-73B5-4CF0-BB93-3ECF5CB7CC84" = "Block Office applications from injecting code into other processes";
+    "26190899-1602-49E8-8B27-EB1D0A1CE869" = "Block Office communication application from creating child processes";
+    "E6DB77E5-3DF2-4CF1-B95A-636979351E5B" = "Block persistence through WMI event subscription";
+    "D1E49AAC-8F56-4280-B9BA-993A6D77406C" = "Block process creations originating from PSExec and WMI commands";
+    "92E97FA1-2EDF-4476-BDD6-9DD0B4DDDC7B" = "Block Win32 API calls from Office macros";
+    "C1DB55AB-C21A-4637-BB3F-A12568109D35" = "Use advanced protection against ransomware";
+    "A8F5898E-1DC8-49A9-9878-85004B8A61E6" = "Block Webshell creation for Servers";
 )
 
 foreach ($rule in $asrRules) {
