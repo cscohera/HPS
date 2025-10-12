a28ce2874209f8582f9cd268a69dba05789873e3
Carson Scohera <155030761+cscohera@users.noreply.github.com>
2025-10-11 21:45:33 -0400

    work?

--- powershell_scripts/hardensavior2022.ps1
+++ powershell_scripts/hardensavior2022.ps1
@@ -7,7 +7,6 @@ reg export "HKLM\SYSTEM\CurrentControlSet\Control\Lsa" C:\temp\lsa_before.reg /y
 reg export "HKLM\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters" C:\temp\srv_before.reg /y
 
 
-
 net accounts /minpwlen:12 /maxpwage:60 /lockoutthreshold:5 /lockoutduration:30 /lockoutwindow:30 | Out-Null
 
 # =============== BLOCK STARTUP FOLDER EXECUTION ===============
@@ -24,8 +23,7 @@ Set-ItemProperty -Path $srpPath -Name "DefaultLevel" -Value 0x40000
 # Disallow %APPDATA% and %TEMP% execution
 $disallowedPaths = @(
     "%AppData%\*",
-    "%LocalAppData%\Temp\*",
-    "%UserProfile%\Downloads\*"
+    "%LocalAppData%\Temp\*"
 )
 
 $ruleID = 262144
@@ -56,29 +54,34 @@ Write-Host "Enabled key audit policies"
 
 # =============== CONFIGURE DEFENDER ASR RULES ===============
 # Ensure Defender is installed and running
-$asrRules = @(
 
-    "56A863A9-875E-4185-98A7-B882C64B5CE5" = "Block abuse of exploited vulnerable signed drivers";
-    "7674BA52-37EB-4A4F-A9A1-F0F9A1619A2C" = "Block Adobe Reader from creating child processes";
-    "D4F940AB-401B-4EFC-AADC-AD5F3C50688A" = "Block all Office applications from creating child processes";
-    "9E6C4E1F-7D60-472F-BA1A-A39EF669E4B2" = "Block credential stealing from the Windows local security authority subsystem (lsass.exe)";
-    "BE9BA2D9-53EA-4CDC-84E5-9B1EEEE46550" = "Block executable content from email client and webmail";
-    "D3E037E1-3EB8-44C8-A917-57927947596D" = "Block JavaScript or VBScript from launching downloaded executable content";
-    "3B576869-A4EC-4529-8536-B80A7769E899" = "Block Office applications from creating executable content";
-    "75668C1F-73B5-4CF0-BB93-3ECF5CB7CC84" = "Block Office applications from injecting code into other processes";
-    "26190899-1602-49E8-8B27-EB1D0A1CE869" = "Block Office communication application from creating child processes";
-    "E6DB77E5-3DF2-4CF1-B95A-636979351E5B" = "Block persistence through WMI event subscription";
-    "D1E49AAC-8F56-4280-B9BA-993A6D77406C" = "Block process creations originating from PSExec and WMI commands";
-    "92E97FA1-2EDF-4476-BDD6-9DD0B4DDDC7B" = "Block Win32 API calls from Office macros";
-    "C1DB55AB-C21A-4637-BB3F-A12568109D35" = "Use advanced protection against ransomware";
-    "A8F5898E-1DC8-49A9-9878-85004B8A61E6" = "Block Webshell creation for Servers";
-)
+Add-MpPreference -AttackSurfaceReductionRules_Ids $rule -AttackSurfaceReductionRules_Actions Enabled
 
-foreach ($rule in $asrRules) {
-    Add-MpPreference -AttackSurfaceReductionRules_Ids $rule -AttackSurfaceReductionRules_Actions Enabled
+    :: Block Office Child Process Creation 
+Add-MpPreference -AttackSurfaceReductionRules_Ids D4F940AB-401B-4EFC-AADC-AD5F3C50688A -AttackSurfaceReductionRules_Actions Enabled
+:: Block Process Injection
+Add-MpPreference -AttackSurfaceReductionRules_Ids 75668C1F-73B5-4CF0-BB93-3ECF5CB7CC84 -AttackSurfaceReductionRules_Actions Enabled
+:: Block Win32 API calls in macros
+Add-MpPreference -AttackSurfaceReductionRules_Ids 92E97FA1-2EDF-4476-BDD6-9DD0B4DDDC7B -AttackSurfaceReductionRules_Actions Enabled
+:: Block Office from creating executables
+Add-MpPreference -AttackSurfaceReductionRules_Ids 3B576869-A4EC-4529-8536-B80A7769E899 -AttackSurfaceReductionRules_Actions Enabled
+:: Block execution of potentially obfuscated scripts
+Add-MpPreference -AttackSurfaceReductionRules_Ids 5BEB7EFE-FD9A-4556-801D-275E5FFC04CC -AttackSurfaceReductionRules_Actions Enabled
+:: Block executable content from email client and webmail
+Add-MpPreference -AttackSurfaceReductionRules_Ids BE9BA2D9-53EA-4CDC-84E5-9B1EEEE46550 -AttackSurfaceReductionRules_Actions Enabled
+:: Block JavaScript or VBScript from launching downloaded executable content
+Add-MpPreference -AttackSurfaceReductionRules_Ids D3E037E1-3EB8-44C8-A917-57927947596D -AttackSurfaceReductionRules_Actions Enabled
+:: Block lsass cred theft
+Add-MpPreference -AttackSurfaceReductionRules_Ids 9e6c4e1f-7d60-472f-ba1a-a39ef669e4b2 -AttackSurfaceReductionRules_Actions Enabled
+:: Block Adobe Reader from creating child processes
+Add-MpPreference -AttackSurfaceReductionRules_Ids 7674ba52-37eb-4a4f-a9a1-f0f9a1619a2c -AttackSurfaceReductionRules_Actions Enabled
+:: Block persistence through WMI event subscription
+Add-MpPreference -AttackSurfaceReductionRules_Ids e6db77e5-3df2-4cf1-b95a-636979351e5b -AttackSurfaceReductionRules_Actions Enabled
+:: Block process creations originating from PSExec and WMI commands
+Add-MpPreference -AttackSurfaceReductionRules_Ids d1e49aac-8f56-4280-b9ba-993a6d77406c -AttackSurfaceReductionRules_Actions Enabled
+
+
     Write-Host "Enabled Defender ASR rule: $rule"
-}
-
 # =============== DISABLE MACROS IN OFFICE (REGISTRY) ===============
 $officeVersions = @("16.0", "15.0", "14.0") # Office 2016, 2013, 2010
 foreach ($ver in $officeVersions) {
