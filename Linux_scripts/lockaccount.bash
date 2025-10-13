e9bdc3ebccb02be8f266b04715d3debaa4a0303a
Carson Scohera <155030761+cscohera@users.noreply.github.com>
2025-10-12 20:40:00 -0400

    final update second place

--- Linux_scripts/lockaccount.bash
+++ Linux_scripts/lockaccount.bash
@@ -1,1 +1,2 @@
+#!/bin/bash
 for u in $(awk -F: '$1!="root" && $3<1000 && $7!~/nologin|false/ {print $1}' /etc/passwd); do passwd -l $u; done
--- Linux_scripts/startup.bash
+++ Linux_scripts/startup.bash
@@ -1,6 +1,4 @@
-
-
-
+#!/bin/bash
 
 
 echo "PermitRootLogin no" >> /etc/ssh/sshd_config
